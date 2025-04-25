373240da596361a2b4635312fac24f3417ffbe24
PanchamKumarr <pancham8256@gmail.com>
2025-04-25 12:36:42 +0530

    Fix: Update apostrophes to HTML entities and fix image paths

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Pancham Kumar",
   profile: '/Images/myImage.JPG',
   designation: "Software Developer",
-  description: "My name is Pancham Kumar. I am a passionate and self-motivated software developer with a strong problem-solving mindset. I enjoy learning new technologies and applying them to build innovative solutions. My primary expertise lies in JavaScript and I work on both frontend and backend development. I'm always looking for opportunities to enhance my skills and contribute to impactful projects.",
+  description: "My name is Pancham Kumar. I am a passionate and self-motivated software developer with a strong problem-solving mindset. I enjoy learning new technologies and applying them to build innovative solutions. My primary expertise lies in JavaScript and I work on both frontend and backend development. I&apos;m always looking for opportunities to enhance my skills and contribute to impactful projects.",
   email: 'pancham8256@gmail.com',
   phone: '6395129477',
   address: 'Behind R.V.I.C Block Colong Khair Aligarh UP 202138',
